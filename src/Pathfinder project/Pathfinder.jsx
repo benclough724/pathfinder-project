35171676d0c0d883564964b35f274d4264b7b144
Ben Clough <ben_clough123@outlook.com>
2020-08-27 17:44:31 +0100

    Its a start
    Me trying to wrap my head around what i need to do!

---
+++ src/Pathfinder project/Pathfinder.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Node from './Node/Node';
+
+import './Pathfinder.css';
+
+function app()
+{
+    <div>
+        <p>
+            hello
+        </p>
+    </div>
+}
