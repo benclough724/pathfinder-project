062bac5b37d78c5b24aaadb38040753c7ba3b3f7
Ben Clough <ben_clough123@outlook.com>
2020-08-29 10:13:27 +0100

    Start on the grid
    In this I'm trying to make my grid to do this im using for loops to represent the collumns and rows and then just looping however many times i need the row or collumns to be in length/size

--- src/Pathfinder/Pathfinder.jsx
+++ src/Pathfinder/Pathfinder.jsx
@@ -13,7 +13,18 @@ export default class Pathfinder extends Component
 }
 }
 
+grid() 
+{
+    const grid = [];
+    for(row = 0; row < 30; row++){
+        const grid
+        for(col = 0; col < 60; col++){
 
+        }
+        grid.push(CurrentRow)
+    }
+    return grid;    
+};
 
 
 
